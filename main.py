285713c9f9e9793396972affe1c4f19cd9574642
Tommy1992 <29047092+Tommy1992@users.noreply.github.com>
2021-12-15 21:14:23 +0000

    some tests hwy len(display) is way too hight, answer is: str(dis) makes every smybol count in len(dis)... every , ' and [] too...

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ guess = input("Guess a letter: ").lower()
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
 chosen_word = list(chosen_word)
-print (chosen_word)
+
 n=-1
 for letter in chosen_word:
   n += 1
@@ -30,9 +30,12 @@ for letter in chosen_word:
     print("Right")
   else:
     print("Wrong")
-print (display)
+
 #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
-print (str(display))
+print (len(display))
 display = str(display)
 print (display)
+print (len(display))
+for i in range(1,len(display)):
+  print ([i])
